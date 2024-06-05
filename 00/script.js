e0edbf79dddae2106564d0ecdd683dcc9305f4e0
YagoABSC <yagoabscosta@gmail.com>
2024-06-04 21:59:36 -0300

    Ainda sem dinheiro, mas agora com cheque especial (conta corrente) e aplicando (conta poupança)

--- 00/script.js
+++ 00/script.js
@@ -29,6 +29,48 @@ class Conta{
     }
 }
 
+class ContaCorrente extends Conta{
+    constructor(numero, saldo = 0, limiteChequeEspecial = 200){
+        super(numero, saldo);
+        this.limiteChequeEspecial = limiteChequeEspecial
+    }
+
+    sacar(valor){
+        const valorDisponivelSaque = this.saldo + this.limiteChequeEspecial;
+
+        if (valor > 0 && valorDisponivelSaque >= valor) {
+            this.saldo -= valor;
+        }else{
+            console.log("Saldo insuficiente.");
+        }
+    }
+}
+
+class ContaPoupanca extends Conta{
+    constructor(numero, saldo = 0, taxaRendimento = 0.2){
+        super(numero, saldo);
+        this.taxaRendimento = taxaRendimento
+    }
+
+    aplicarRendimento(valor){
+        console.log("Rendimento aplicado");
+        const aplicarRendi = this.saldo * this.taxaRendimento;
+
+        if (valor > 0 && valor >= this.saldo) {
+            
+        }
+    }
+}
+
+let contaC = new ContaCorrente(2001, 500);
+
+contaC.sacar(700);
+console.log(contaC.toString());
+
+let contaP = new ContaPoupanca(2002,500);
+contaP.aplicarRendimento();
+// contaC.aplicarRendimento(); erro por não existir função nessa classe
+
 // const conta1 = new Conta(1001, 50);
 // const conta2 = new Conta(1002);
 
